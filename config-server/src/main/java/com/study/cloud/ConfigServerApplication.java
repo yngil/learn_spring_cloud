26a23363e2a9748b7074823f462a739e3c156efe
yangyi@hexindai.com <yangyi@hexindai.com>
2019-08-06 17:55:28 +0800

    add config commit

--- config-server/src/main/java/com/study/cloud/ConfigServerApplication.java
+++ config-server/src/main/java/com/study/cloud/ConfigServerApplication.java
@@ -3,10 +3,11 @@ package com.study.cloud;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
-@EnableEurekaServer
+@EnableEurekaClient
 @EnableConfigServer
 public class ConfigServerApplication {
     public static void main(String[] args) {
--- eureka-server/src/main/java/com/study/cloud/EurekaServerApplication.java
+++ eureka-server/src/main/java/com/study/cloud/EurekaServerApplication.java
@@ -2,11 +2,14 @@ package com.study.cloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
-public class ConfigClientApplication {
-    public static void main(String[] args) {
-        SpringApplication.run(ConfigClientApplication.class);
-    }
-}
+@EnableEurekaServer
+public class EurekaServerApplication {
 
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServerApplication.class, args);
+    }
+
+}
