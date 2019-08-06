3611bfa6dd9d13b5dc2a62b5475219155109bf98
yangyi@hexindai.com <yangyi@hexindai.com>
2019-08-06 15:11:18 +0800

    add config properties

--- config-server/src/main/java/com/study/cloud/ConfigServerApplication.java
+++ config-server/src/main/java/com/study/cloud/ConfigServerApplication.java
@@ -2,10 +2,12 @@ package com.study.cloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @EnableEurekaServer
+@EnableConfigServer
 public class ConfigServerApplication {
     public static void main(String[] args) {
         SpringApplication.run(ConfigServerApplication.class);
